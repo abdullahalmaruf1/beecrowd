710e1705be457bdbf18e3eee202a56361cbba57c
abdullahalmaruf1 <138432899+abdullahalmaruf1@users.noreply.github.com>
2023-07-17 11:02:14 +0600

    Update 1019timeconversion.c
    found another method

--- 1019timeconversion.c
+++ 1019timeconversion.c
@@ -13,6 +13,23 @@ int main() {
     //printing hours,minutes & seconds
     printf("%d:%d:%d\n", hours,minutes,seconds);
     
+    return 0;
+
+}
+// another method
+
+#include<stdio.h>
+
+int main() {
+    int seconds, minutes, hours;
+    scanf("%d",&seconds);
+    //findingout hours,minutes & seconds
+    hours = seconds/3600;
+    minutes = seconds%3600/60;
+    seconds =seconds%3600%60;
+    //printing hours,minutes & seconds
+    printf("%d:%d:%d\n", hours,minutes,seconds);
+    
     
     
     return 0;
